ab2382b8497fad4992e24e7090b9791da63bab9d
Fran Bec <franbecvort@gmail.com>
2024-11-29 21:20:26 +0000

    initial commit

---
+++ src/main/groovy/dev/pollito/roundest_groovy/RoundestGroovyApplication.groovy
@@ -0,0 +1,13 @@
+package dev.pollito.roundest_groovy
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class RoundestGroovyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(RoundestGroovyApplication, args)
+	}
+
+}
---
+++ src/test/groovy/dev/pollito/roundest_groovy/RoundestGroovyApplicationTests.groovy
@@ -0,0 +1,13 @@
+package dev.pollito.roundest_groovy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoundestGroovyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
