227a456299a34d0f00ef7132baaebe96acc44250
Fran Bec <franbecvort@gmail.com>
2024-12-03 16:52:42 +0000

    temp fix, mapper does not work with groovy interface, but yep with java interface

--- src/main/groovy/dev/pollito/roundest_groovy/mapper/PokemonModelMapper.java
+++ src/main/groovy/dev/pollito/roundest_groovy/mapper/PokemonModelMapper.java
@@ -1,12 +1,12 @@
-package dev.pollito.roundest_groovy.mapper
+package dev.pollito.roundest_groovy.mapper;
 
-import dev.pollito.roundest_groovy.entity.Pokemon
-import dev.pollito.roundest_groovy.model.Pokemons
-import org.mapstruct.Mapper
-import org.mapstruct.MappingConstants
-import org.springframework.data.domain.Page
+import dev.pollito.roundest_groovy.entity.Pokemon;
+import dev.pollito.roundest_groovy.model.Pokemons;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.springframework.data.domain.Page;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 interface PokemonModelMapper {
-    Pokemons map(Page<Pokemon> pokemonPage)
+    Pokemons map(Page<Pokemon> pokemonPage);
 }
