3714d61c6616d68f5d898531f7c0bb4e42fd0b1f
Fran Bec <franbecvort@gmail.com>
2024-12-18 18:25:17 +0000

    X-Application-Name

---
+++ src/main/groovy/dev/pollito/roundest_groovy/filter/ApplicationNameHeaderFilter.groovy
@@ -0,0 +1,31 @@
+package dev.pollito.roundest_groovy.filter
+
+import dev.pollito.roundest_groovy.configuration.properties.SpringApplicationConfigurationProperties
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletException
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+
+@Component
+@Order(1)
+class ApplicationNameHeaderFilter implements Filter {
+
+	private final SpringApplicationConfigurationProperties springApplicationConfigurationProperties
+
+	ApplicationNameHeaderFilter(SpringApplicationConfigurationProperties springApplicationConfigurationProperties){
+		this.springApplicationConfigurationProperties = springApplicationConfigurationProperties
+	}
+
+	void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+	throws IOException, ServletException {
+		if (response instanceof HttpServletResponse) {
+			HttpServletResponse httpServletResponse = (HttpServletResponse) response
+			httpServletResponse.addHeader("X-Application-Name", springApplicationConfigurationProperties.name)
+		}
+		chain.doFilter(request, response)
+	}
+}
--- src/main/groovy/dev/pollito/roundest_groovy/filter/LogFilter.groovy
+++ src/main/groovy/dev/pollito/roundest_groovy/filter/LogFilter.groovy
@@ -7,7 +7,11 @@ import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
 
+@Component
+@Order(2)
 @Slf4j
 class LogFilter implements Filter {
 
